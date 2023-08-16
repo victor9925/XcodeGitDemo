a8dff4997b9017c5f7bb06a15a8d30bf621b7064
Victor <victor.gamboa2@correo.usa.edu.co>
2023-08-15 22:53:57 -0500

    secoundView

---
+++ github_1/SecoundView.swift
@@ -0,0 +1,20 @@
+//
+//  SecoundView.swift
+//  github_1
+//
+//  Created by victor on 15/08/23.
+//
+
+import SwiftUI
+
+struct SecoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecoundView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecoundView()
+    }
+}
