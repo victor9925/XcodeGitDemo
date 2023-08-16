fdaa8b9f7f46eddfadad9bbadfc7a392095105f8
Victor <victor.gamboa2@correo.usa.edu.co>
2023-08-15 22:16:13 -0500

    Initial Commit

---
+++ github_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github_1
+//
+//  Created by victor on 15/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github_1/github_1App.swift
@@ -0,0 +1,17 @@
+//
+//  github_1App.swift
+//  github_1
+//
+//  Created by victor on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct github_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
