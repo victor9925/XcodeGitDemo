43c8cda90127c61ba7086554f556f368d5679ed4
Victor <victor.gamboa2@correo.usa.edu.co>
2023-09-07 00:52:59 -0500

    mi primer interaccionara con stacks

--- github_1/mainview.swift
+++ github_1/mainview.swift
@@ -13,15 +13,63 @@ import SwiftUI
 
 struct mainview: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 0
+               ) {
             Text("First commit")
                 .font(.title)
-                .foregroundColor(Color.green)
+                .foregroundColor(Color.white)
+                .background(Color.cyan)
+            Spacer()
             
-            Text("este es mi primer playground")
-
+            ZStack {
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.blue)
+                    .padding(10)
+                
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.gray)
+                    .padding(20)
+                
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.yellow)
+                    .padding(35)
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.brown)
+                    .padding(.bottom, 150)
+                    .padding(.top,150)
+                
+                Text("Dentro de dos Spaces")
+                    .font(.body)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.yellow)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
+                
+            }
+                
+            Spacer()
             
+            HStack {
+                Text("este es mi primer playground")
+                
+                Spacer()
+                
+                Text("Este es mi primer playground")
+                    .font(.body)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.yellow)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
+            }
         }
+               .padding()
+               .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
        
     }
 }
